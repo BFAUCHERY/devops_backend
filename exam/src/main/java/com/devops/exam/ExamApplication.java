fc9fe5ae8a24d6849f0f208fe17e514015f43a16
benoit.fauchery <Benoit.Fauchery@cgi.com>
2024-07-23 14:39:18 +0200

    Allow CORS

--- exam/src/main/java/com/devops/exam/ExamApplication.java
+++ exam/src/main/java/com/devops/exam/ExamApplication.java
@@ -5,12 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*") // Permettre toutes les origines
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
