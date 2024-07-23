d3b5dcc723377a71a548f008c082331e3a2c3885
benoit.fauchery <Benoit.Fauchery@cgi.com>
2024-07-23 14:29:22 +0200

    Allow CORS

--- exam/src/main/java/com/devops/exam/Controller/PersonController.java
+++ exam/src/main/java/com/devops/exam/Controller/PersonController.java
@@ -2,6 +2,7 @@ package com.devops.exam.Controller;
 
 import com.devops.exam.Entity.Person;
 import com.devops.exam.Repository.PersonRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/persons")
+@CrossOrigin(origins = "*")
 public class PersonController {
 	
 	private final PersonRepository personRepository;
